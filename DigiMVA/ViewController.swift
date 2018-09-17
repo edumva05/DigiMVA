3955c900e9af040994b43786ba5c729d5e22cd8a
EDUMVA5 <edumva5@mva53s-imac.local>
2018-09-17 16:32:35 +0530

    Initial Commit

---
+++ DigiMVA/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DigiMVA
+//
+//  Created by EDUMVA5 on 17/09/18.
+//  Copyright © 2018 DigiMVA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
